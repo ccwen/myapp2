c87693257a36a98885dc95f9f0d8576dc635377e
yapcheahshen <yapcheahshen@gmail.com>
2015-03-10 15:17:16 +0800

    first commit

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp2",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/yapcheahshen/myapp2/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  144673,
+  277980,
+  389,
+  35607,
+  113502,
+  97570
+ ],
+ "filedates": [
+  "2015-03-10T06:55:08.112Z",
+  "2015-03-10T06:55:08.110Z",
+  "2015-03-10T06:55:08.248Z",
+  "2015-03-10T06:55:18.875Z",
+  "2015-03-10T06:55:08.164Z",
+  "2015-03-10T06:55:08.167Z",
+  "2015-03-10T06:55:08.108Z",
+  "2015-03-10T06:55:08.090Z",
+  "2015-03-10T06:55:12.187Z"
+ ],
+ "date": "2015-03-10T06:55:19.846Z"
+})
